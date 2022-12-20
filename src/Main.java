28261b92d16b055a7ffd969879ef27a15fd3f35b
АндрейПетухов1986 <oziglena@mail.ru>
2022-12-20 22:12:27 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("задача 1");
+        System.out.println("dog =");
+        System.out.println(dog);
+        System.out.println("cat =");
+        System.out.println(cat);
+        System.out.println("paper =");
+        System.out.println(paper);
+        System.out.println("задача 2");
+        var a = 4;
+        dog = dog + a;
+        cat = cat + a;
+        paper = paper + a;
+        System.out.println("dog =");
+        System.out.println(dog);
+        System.out.println("cat =");
+        System.out.println(cat);
+        System.out.println("paper =");
+        System.out.println(paper);
+        System.out.println("задача 3");
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("dog =");
+        System.out.println(dog);
+        System.out.println("cat =");
+        System.out.println(cat);
+        System.out.println("paper =");
+        System.out.println(paper);
+        System.out.println("задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        System.out.println("задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        System.out.println("задача 6");
+        var Ivan = 78.2;
+        var Oleg = 82.7;
+        var totalWeight = Ivan + Oleg;
+        System.out.println("Вес Ивана" + Ivan + "кг");
+        System.out.println("Вес Олега" + Oleg + "кг");
+        System.out.println("общий вес спортсменнов" + totalWeight + "кг");
+        var difference = Oleg - Ivan;
+        System.out.println("разница между бойцами" + difference + "кг");
+        var x = Oleg % Ivan;
+        System.out.println("разница между бойцами" + x + "кг");
+
+    }
+}
